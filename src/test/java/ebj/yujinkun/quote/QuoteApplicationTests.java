711ef4d6d50222ce662d3b74f66761b88639a0db
Yudzeen <eugenejavinas@gmail.com>
2020-02-25 14:34:24 +0800

    Initial commit

---
+++ src/test/java/ebj/yujinkun/quote/QuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ebj.yujinkun.quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
